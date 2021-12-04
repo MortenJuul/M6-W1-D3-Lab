14a353b1287e8368b831dd5feb5ed6794e2539a7
MortenJuul <54556492+MortenJuul@users.noreply.github.com>
2021-12-04 15:01:22 -0800

    Complete

--- crud-json-server/src/Lists.js
+++ crud-json-server/src/Lists.js
@@ -1,31 +1,51 @@
-import React from 'react';
+import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import UpdateList from "./UpdateList";
+import DeleteList from "./DeleteList";
 
-function Lists(props) {
-    let listrows = [];
-    props.alldata.forEach(element =>{
-        listrows.push(
-            <tr key={element.id}>
-                <td>{element.id}</td>
-                <td>{element.title}</td>
-                <td>{element.author}</td>
-            </tr>
-        )
-    })
-    return (
-        <table className="table table-striped">
-            <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Title</th>
-                    <th>Author</th>
-                    <th>Update</th>
-                    <th>Delete</th>
-                </tr>
-            </thead>
-            <tbody>{listrows}</tbody>
-        </table>
+const Lists = (props) => {
+  console.log(props);
+  let listrows = [];
+  props.alldata.forEach((ele) => {
+    listrows.push(
+      <tr key={ele.id}>
+        <td>{ele.id}</td>
+        <td>{ele.title}</td>
+        <td>{ele.author}</td>
+        <td>
+          <UpdateList
+            elementId={ele.id}
+            singledata={props.singledata}
+            getList={props.getList}
+            updateList={props.updateList}
+            handleChange={props.handleChange}
+          />
+        </td>
+        <td>
+          <DeleteList
+            elementId={ele.id}
+            singledata={props.singledata}
+            getList={props.getList}
+            deleteList={props.deleteList}
+          />
+        </td>
+      </tr>
     );
-}
+  });
+  return (
+    <table className="table table-striped">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Title</th>
+          <th>Author</th>
+          <th>Update</th>
+          <th>Delete</th>
+        </tr>
+      </thead>
+      <tbody>{listrows}</tbody>
+    </table>
+  );
+};
 
 export default Lists;
